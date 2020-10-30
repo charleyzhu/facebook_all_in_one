348f3d837446ad8e7bad3c9a2ee18bfd82a17904
Charley <2555085@qq.com>
2020-10-30 17:26:02 +0800

    android deep link

--- android/src/main/java/com/vistateam/facebook_all_in_one/FacebookAllInOnePlugin.java
+++ android/src/main/java/com/vistateam/facebook_all_in_one/FacebookAllInOnePlugin.java
@@ -1,39 +1,130 @@
 package com.vistateam.facebook_all_in_one;
 
+import android.app.Activity;
+import android.content.Context;
+
+import com.facebook.FacebookSdk;
+import com.facebook.applinks.AppLinkData;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** FacebookAllInOnePlugin */
+import android.os.Handler;
+import android.util.Log;
+
+/**
+ * FacebookAllInOnePlugin
+ */
 public class FacebookAllInOnePlugin implements FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private MethodChannel channel;
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
 
-  @Override
-  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "facebook_all_in_one");
-    channel.setMethodCallHandler(this);
-  }
+    // Channel String;
+    private static final String CHANNEL_NAME = "VistaTeam/facebook_all_in_one";
 
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+    private Context mContext;
+    private Activity mActivity;
+
+//    private FacebookAuth facebookAuth = new FacebookAuth();
+    private ActivityPluginBinding activityPluginBinding;
+
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), CHANNEL_NAME);
+        channel.setMethodCallHandler(this);
+        this.mContext = flutterPluginBinding.getApplicationContext();
     }
-  }
 
-  @Override
-  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
-    channel.setMethodCallHandler(null);
-  }
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else if (call.method.equals("getFBLinks")) {
+            getFBLinks(call, result);
+        }  else {
+            result.notImplemented();
+        }
+    }
+
+    private void getFBLinks(MethodCall call, Result result) {
+        final Map<String, String> data = new HashMap<>();
+        final Result resultDelegate = result;
+        // Get a handler that can be used to post to the main thread
+        final Handler mainHandler = new Handler(mContext.getMainLooper());
+
+        // Get user consent
+        FacebookSdk.setAutoInitEnabled(true);
+        FacebookSdk.fullyInitialize();
+        AppLinkData.fetchDeferredAppLinkData(mContext,
+                new AppLinkData.CompletionHandler() {
+                    @Override
+                    public void onDeferredAppLinkDataFetched(AppLinkData appLinkData) {
+                        // Process app link data
+                        if (appLinkData != null) {
+
+                            if (appLinkData.getTargetUri() != null) {
+                                //Log.d("FB_APP_LINKS", "Deferred Deeplink Received: " + appLinkData.getTargetUri().toString());
+                                data.put("deeplink", appLinkData.getTargetUri().toString());
+                            }
+
+                            //Log.d("FB_APP_LINKS", "Deferred Deeplink Received: " + appLinkData.getPromotionCode());
+                            if (appLinkData.getPromotionCode() != null)
+                                data.put("promotionalCode", appLinkData.getPromotionCode());
+                            else
+                                data.put("promotionalCode", "");
+
+                            Runnable myRunnable = new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (resultDelegate != null)
+                                        resultDelegate.success(data);
+                                }
+                            };
+
+                            mainHandler.post(myRunnable);
+
+                        } else {
+                            //Log.d("FB_APP_LINKS", "Deferred Deeplink Received: null link");
+
+                            Runnable myRunnable = new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (resultDelegate != null)
+                                        resultDelegate.success(null);
+                                }
+                            };
+
+                            mainHandler.post(myRunnable);
+
+                        }
+
+                    }
+                }
+        );
+    }
+
+
 }
