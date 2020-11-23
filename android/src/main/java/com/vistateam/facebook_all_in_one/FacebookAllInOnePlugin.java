d122d5c732950fcbd78d51ccd27be97d945fc752
Charley <2555085@qq.com>
2020-11-23 18:05:28 +0800

    getLaunchingLink

--- android/src/main/java/com/vistateam/facebook_all_in_one/FacebookAllInOnePlugin.java
+++ android/src/main/java/com/vistateam/facebook_all_in_one/FacebookAllInOnePlugin.java
@@ -1,24 +1,15 @@
 package com.vistateam.facebook_all_in_one;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.os.Bundle;
+import android.content.Intent;
 import android.os.Handler;
 
 import com.facebook.FacebookSdk;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.applinks.AppLinkData;
-import com.facebook.login.LoginBehavior;
 
-import org.json.JSONObject;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Currency;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -26,49 +17,54 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
 
 /**
  * FacebookAllInOnePlugin
  */
-public class FacebookAllInOnePlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+public class FacebookAllInOnePlugin implements FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.NewIntentListener,EventChannel.StreamHandler {
     // Channel String;
     private static final String CHANNEL_NAME = "VistaTeam/facebook_all_in_one";
-    private static final String ERROR_UNKNOWN_LOGIN_BEHAVIOR = "unknown_login_behavior";
-    private static final String LOGIN_BEHAVIOR_NATIVE_WITH_FALLBACK = "nativeWithFallback";
-    private static final String LOGIN_BEHAVIOR_NATIVE_ONLY = "nativeOnly";
-    private static final String LOGIN_BEHAVIOR_WEB_ONLY = "webOnly";
-    private static final String LOGIN_BEHAVIOR_WEB_VIEW_ONLY = "webViewOnly";
-    private static final String ARG_LOGIN_BEHAVIOR = "behavior";
-    private static final String ARG_PERMISSIONS = "permissions";
-    private static final String ARG_FIELDS = "fields";
-    private static final String ARG_DEEP_LINK = "deeplink";
-    private static final String ARG_PROMOTIONAL_CODE = "promotionalCode";
-    private static final String METHOD_DEEP_LINK = "getFBLinks";
-    private static final String METHOD_LOG_IN = "login";
-    private static final String METHOD_IS_LOGGED = "isLogged";
-    private static final String METHOD_GET_USER_DATA = "getUserData";
-    private static final String METHOD_LOG_OUT = "logOut";
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
     private Context mContext;
-    AppEventsLogger appEventsLogger ;
     private Activity mActivity;
+
     private FacebookAuth facebookAuth = new FacebookAuth();
     private ActivityPluginBinding activityPluginBinding;
+
+    private String initialLink;
+    private String latestLink;
+    private boolean initialIntent = true;
+    private BroadcastReceiver changeReceiver;
+
+    private static final String MESSAGES_CHANNEL = "uni_links/messages";
+    private static final String EVENTS_CHANNEL = "uni_links/events";
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
         channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), CHANNEL_NAME);
         channel.setMethodCallHandler(this);
         this.mContext = flutterPluginBinding.getApplicationContext();
-        appEventsLogger = AppEventsLogger.newLogger(mContext);
+        register(flutterPluginBinding.getFlutterEngine().getDartExecutor(), this);
+
+    }
+
+    private static void register(BinaryMessenger messenger, FacebookAllInOnePlugin plugin) {
+        final MethodChannel methodChannel = new MethodChannel(messenger, MESSAGES_CHANNEL);
+        methodChannel.setMethodCallHandler(plugin);
+
+        final EventChannel eventChannel = new EventChannel(messenger, EVENTS_CHANNEL);
+        eventChannel.setStreamHandler(plugin);
     }
 
     @Override
@@ -81,45 +77,21 @@ public class FacebookAllInOnePlugin implements FlutterPlugin, MethodCallHandler,
 
         if (call.method.equals("getPlatformVersion")) {
             result.success("Android " + android.os.Build.VERSION.RELEASE);
-        } else if (call.method.equals(METHOD_DEEP_LINK)) {
+        } else if (call.method.equals("getFBLinks")) {
             getFBLinks(call, result);
-        } else if (call.method.equals(METHOD_LOG_IN)) {
-            List<String> permissions = call.argument(ARG_PERMISSIONS);
-            String loginBehaviorStr = call.argument(ARG_LOGIN_BEHAVIOR);
-            LoginBehavior loginBehavior = loginBehaviorFromString(loginBehaviorStr, result);
-            facebookAuth.login(this.activityPluginBinding.getActivity(), loginBehavior, permissions, result);
-        } else if (call.method.equals(METHOD_IS_LOGGED)) {
+        } else if (call.method.equals("login")) {
+            List<String> permissions = call.argument("permissions");
+            facebookAuth.login(this.activityPluginBinding.getActivity(), permissions, result);
+        } else if (call.method.equals("isLogged")) {
             facebookAuth.isLogged(result);
-        } else if (call.method.equals(METHOD_GET_USER_DATA)) {
-            String fields = call.argument(ARG_FIELDS);
+        } else if (call.method.equals("getUserData")) {
+            String fields = call.argument("fields");
             facebookAuth.getUserData(fields, result);
-        } else if (call.method.equals(METHOD_LOG_OUT)) {
+        } else if (call.method.equals("logOut")) {
             facebookAuth.logOut(result);
-        } else if (call.method.equals("clearUserData")) {
-            handleClearUserData(call, result);
-        } else if (call.method.equals("clearUserID")) {
-            handleClearUserId(call, result);
-        }else if (call.method.equals("flush")) {
-            handleFlush(call, result);
-        }else if (call.method.equals("getApplicationId")) {
-            getApplicationId(call, result);
-        }else if (call.method.equals("logEvent")) {
-            handleLogEvent(call, result);
-        }else if (call.method.equals("logPushNotificationOpen")) {
-            handlePushNotificationOpen(call, result);
-        }else if (call.method.equals("setUserData")) {
-            handleSetUserData(call, result);
-        }else if (call.method.equals("setUserID")) {
-            handleSetUserId(call, result);
-        }else if (call.method.equals("updateUserProperties")) {
-            handleUpdateUserProperties(call, result);
-        }else if (call.method.equals("setAutoLogAppEventsEnabled")) {
-            handleSetAutoLogAppEventsEnabled(call, result);
-        }else if (call.method.equals("setDataProcessingOptions")) {
-            setDataProcessingOptions(call, result);
-        }else if (call.method.equals("logPurchase")) {
-            handlePurchased(call, result);
-        }else {
+        } else if (call.method.equals("getInitialLink")) {
+            result.success(initialLink);
+        } else {
             result.notImplemented();
         }
     }
@@ -142,14 +114,14 @@ public class FacebookAllInOnePlugin implements FlutterPlugin, MethodCallHandler,
 
                             if (appLinkData.getTargetUri() != null) {
                                 //Log.d("FB_APP_LINKS", "Deferred Deeplink Received: " + appLinkData.getTargetUri().toString());
-                                data.put(ARG_DEEP_LINK, appLinkData.getTargetUri().toString());
+                                data.put("deeplink", appLinkData.getTargetUri().toString());
                             }
 
                             //Log.d("FB_APP_LINKS", "Deferred Deeplink Received: " + appLinkData.getPromotionCode());
                             if (appLinkData.getPromotionCode() != null)
-                                data.put(ARG_PROMOTIONAL_CODE, appLinkData.getPromotionCode());
+                                data.put("promotionalCode", appLinkData.getPromotionCode());
                             else
-                                data.put(ARG_PROMOTIONAL_CODE, "");
+                                data.put("promotionalCode", "");
 
                             Runnable myRunnable = new Runnable() {
                                 @Override
@@ -196,6 +168,8 @@ public class FacebookAllInOnePlugin implements FlutterPlugin, MethodCallHandler,
     @Override
     public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
         this.attachToActivity(binding);
+        activityPluginBinding.addOnNewIntentListener(this);
+        this.handleIntent(this.mContext, activityPluginBinding.getActivity().getIntent());
     }
 
     @Override
@@ -206,6 +180,8 @@ public class FacebookAllInOnePlugin implements FlutterPlugin, MethodCallHandler,
     @Override
     public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
         this.attachToActivity(binding);
+        activityPluginBinding.addOnNewIntentListener(this);
+        this.handleIntent(this.mContext, activityPluginBinding.getActivity().getIntent());
     }
 
     @Override
@@ -213,189 +189,68 @@ public class FacebookAllInOnePlugin implements FlutterPlugin, MethodCallHandler,
         disposeActivity();
     }
 
-    private LoginBehavior loginBehaviorFromString(String loginBehavior, Result result) {
-        switch (loginBehavior) {
-            case LOGIN_BEHAVIOR_NATIVE_WITH_FALLBACK:
-                return LoginBehavior.NATIVE_WITH_FALLBACK;
-            case LOGIN_BEHAVIOR_NATIVE_ONLY:
-                return LoginBehavior.NATIVE_ONLY;
-            case LOGIN_BEHAVIOR_WEB_ONLY:
-                return LoginBehavior.WEB_ONLY;
-            case LOGIN_BEHAVIOR_WEB_VIEW_ONLY:
-                return LoginBehavior.WEB_VIEW_ONLY;
-            default:
-                result.error(
-                        ERROR_UNKNOWN_LOGIN_BEHAVIOR,
-                        "setLoginBehavior called with unknown login behavior: "
-                                + loginBehavior,
-                        null
-                );
-                return null;
+    @Override
+    public boolean onNewIntent(Intent intent) {
+        this.handleIntent(mContext, intent);
+        return false;
+    }
+
+    private void handleIntent(Context context, Intent intent) {
+        String action = intent.getAction();
+        String dataString = intent.getDataString();
+
+        if (Intent.ACTION_VIEW.equals(action)) {
+            if (initialIntent) {
+                initialLink = dataString;
+                initialIntent = false;
+            }
+            latestLink = dataString;
+            if (changeReceiver != null) changeReceiver.onReceive(context, intent);
         }
     }
 
-    private void handleClearUserData(MethodCall call, Result result) {
-        AppEventsLogger.clearUserData();
-        result.success(null);
-    }
-
-    private void handleClearUserId(MethodCall call, Result result) {
-        AppEventsLogger.clearUserID();
-        result.success(null);
-    }
-
-    private void handleFlush(MethodCall call, Result result) {
-        appEventsLogger.flush();
-        result.success(null);
-    }
-
-    private void getApplicationId(MethodCall call, Result result) {
-        result.success(appEventsLogger.getApplicationId());
-    }
-
-    private void handleLogEvent(MethodCall call, Result result) {
-        String eventName = call.argument("name");
-        Map<String, Object> parameters = call.argument("parameters");
-        Double valueToSum = call.argument("_valueToSum");
-
-        if (valueToSum != null && parameters != null) {
-            Bundle parameterBundle = createBundleFromMap(parameters);
-            appEventsLogger.logEvent(eventName, valueToSum, parameterBundle);
-        } else if (valueToSum != null) {
-            appEventsLogger.logEvent(eventName, valueToSum);
-        } else if (parameters != null) {
-            Bundle parameterBundle = createBundleFromMap(parameters);
-            appEventsLogger.logEvent(eventName, parameterBundle);
-        } else {
-            appEventsLogger.logEvent(eventName);
-        }
-
-        result.success(null);
-    }
-
-    private void handlePushNotificationOpen(MethodCall call, Result result) {
-        String action = call.argument("action");
-        Map<String, Object> payload = call.argument("payload");
-        Bundle payloadBundle = createBundleFromMap(payload);
-
-        if (action != null) {
-            appEventsLogger.logPushNotificationOpen(payloadBundle, action);
-        } else {
-            appEventsLogger.logPushNotificationOpen(payloadBundle);
-        }
-
-        result.success(null);
-    }
-
-    private void handleSetUserData(MethodCall call, Result result) {
-        Map<String, Object> parameters = call.argument("parameters");
-        Bundle parameterBundle = createBundleFromMap(parameters);
-
-        if (parameterBundle != null) {
-            AppEventsLogger.setUserData(parameterBundle.getString("email")
-                    , parameterBundle.getString("firstName")
-                    , parameterBundle.getString("lastName")
-                    , parameterBundle.getString("phone")
-                    , parameterBundle.getString("dateOfBirth")
-                    , parameterBundle.getString("gender")
-                    , parameterBundle.getString("city")
-                    , parameterBundle.getString("state")
-                    , parameterBundle.getString("zip")
-                    , parameterBundle.getString("country"));
-        }
-
-
-        result.success(null);
-    }
-
-    private void handleUpdateUserProperties(MethodCall call, final Result result) {
-        String applicationId = call.argument("applicationId");
-        Map<String, Object> parameters = call.argument("parameters");
-        Bundle parameterBundle = createBundleFromMap(parameters);
-
-        GraphRequest.Callback requestCallback = new GraphRequest.Callback() {
+    private BroadcastReceiver createChangeReceiver(final EventChannel.EventSink events) {
+        return new BroadcastReceiver() {
             @Override
-            public void onCompleted(GraphResponse response) {
-                JSONObject data = response.getJSONObject();
-                result.success(data);
+            public void onReceive(Context context, Intent intent) {
+                // NOTE: assuming intent.getAction() is Intent.ACTION_VIEW
+
+                // Log.v("uni_links", String.format("received action: %s", intent.getAction()));
+
+                String dataString = intent.getDataString();
+
+                if (dataString == null) {
+                    events.error("UNAVAILABLE", "Link unavailable", null);
+                } else {
+                    events.success(dataString);
+                }
             }
         };
-
-        if (applicationId == null)
-            AppEventsLogger.updateUserProperties(parameterBundle, requestCallback);
-        else AppEventsLogger.updateUserProperties(parameterBundle, applicationId, requestCallback);
-
-        result.success(null);
     }
 
-    private void handleSetUserId(MethodCall call, Result result) {
-        String id = (String) call.arguments;
-        AppEventsLogger.setUserID(id);
-        result.success(null);
+
+    @Override
+    public void onListen(Object arguments, EventChannel.EventSink events) {
+        changeReceiver = createChangeReceiver(events);
     }
 
-    private Bundle createBundleFromMap(Map<String, Object> parameterMap) {
-        if (parameterMap == null) {
-            return null;
+    @Override
+    public void onCancel(Object arguments) {
+        changeReceiver = null;
+    }
+
+    public static void registerWith(PluginRegistry.Registrar registrar) {
+        // Detect if we've been launched in background
+        if (registrar.activity() == null) {
+            return;
         }
 
-        Bundle bundle = new Bundle();
+        final FacebookAllInOnePlugin instance = new FacebookAllInOnePlugin();
+        instance.mContext = registrar.context();
+        register(registrar.messenger(), instance);
 
-        Iterator entries = parameterMap.entrySet().iterator();
-        while (entries.hasNext()) {
-            Map.Entry entry = (Map.Entry) entries.next();
-            Object value = entry.getValue();
-            String key = (String) entry.getKey();
-            if (value instanceof String) {
-                bundle.putString(key, String.valueOf(value));
-            } else if (value instanceof Integer) {
-                bundle.putInt(key, (Integer) value);
-            } else if (value instanceof Long) {
-                bundle.putLong(key, (Long) value);
-            } else if (value instanceof Double) {
-                bundle.putDouble(key, (Double) value);
-            } else if (value instanceof Boolean) {
-                bundle.putBoolean(key, (Boolean) value);
-            } else if (value instanceof Map) {
-                Bundle nestedBundle = createBundleFromMap((Map<String, Object>) value);
-                bundle.putBundle(key, nestedBundle);
-            } else {
-                throw new IllegalArgumentException(
-                        "Unsupported value type: ");
-            }
-
-        }
-
-        return bundle;
-    }
-
-    private void handleSetAutoLogAppEventsEnabled(MethodCall call, Result result) {
-        Boolean enabled = (Boolean) call.arguments;
-        FacebookSdk.setAutoLogAppEventsEnabled(enabled);
-        result.success(null);
-    }
-
-    private void setDataProcessingOptions(MethodCall call, Result result) {
-        ArrayList<String> options = call.argument("options");
-        Object countryObject = call.argument("country");
-        int country = countryObject instanceof Integer ? (int) countryObject : 0;
-        Object stateObject = call.argument("state");
-        int state = stateObject instanceof Integer ? (int) stateObject : 0;
-
-        FacebookSdk.setDataProcessingOptions((String[]) options.toArray(), country, state);
-        result.success(null);
-    }
-
-    private void handlePurchased(MethodCall call, Result result) {
-        Object amountObject = call.argument("amount");
-        Double amount = amountObject instanceof Double ? (Double) amountObject : 0;
-
-        Currency currency = Currency.getInstance(String.valueOf(call.argument("currency")));
-        Map<String, Object> parameters = call.argument("parameters");
-        Bundle parameterBundle = createBundleFromMap(parameters);
-
-        appEventsLogger.logPurchase(BigDecimal.valueOf(amount), currency, parameterBundle);
-        result.success(null);
+        instance.handleIntent(registrar.context(), registrar.activity().getIntent());
+        registrar.addNewIntentListener(instance);
     }
 
 }
