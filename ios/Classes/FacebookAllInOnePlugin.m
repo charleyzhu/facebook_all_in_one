6fa48bd16240e0ac996890a39cede9dbd18e6f33
Charley <2555085@qq.com>
2020-11-25 11:46:43 +0800

    update 修改iOS返回参数

--- ios/Classes/FacebookAllInOnePlugin.m
+++ ios/Classes/FacebookAllInOnePlugin.m
@@ -38,11 +38,14 @@
     if (url) {
         self.launchingLink = [url absoluteString];
     }
-    return [[FBSDKApplicationDelegate sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+    
+    [[FBSDKApplicationDelegate sharedInstance] application:application didFinishLaunchingWithOptions:launchOptions];
+    return YES;
 }
 
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(nonnull NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options {
-    return [[FBSDKApplicationDelegate sharedInstance] application:application openURL:url options:options];
+    [[FBSDKApplicationDelegate sharedInstance] application:application openURL:url options:options];
+    return YES;
 }
 
 
